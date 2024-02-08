6b40438729d6027e0fb9362d9bc9056e5dae4052
Jayvee <jayveemaniquiz0123@gmail.com>
2024-02-08 22:42:21 +0800

    WebPack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import numbering from "./messages";
+import './css/style.css'
+
+
+console.log( numbering.reduce((x,y) => x+y))
---
+++ src/messages.js
@@ -0,0 +1,4 @@
+const numbering = [1,2,3,4,5,6];
+
+
+export default numbering;
